deb069d8482fefecec58951c139366dbf93e4ff2
Liam <G00420041@atu.ie>
2024-04-10 16:59:36 +0100

    added testcase for subtract , multiply, and divide

--- src/main/java/ie/atu/calc/Calculator.java
+++ src/main/java/ie/atu/calc/Calculator.java
@@ -1,8 +1,22 @@
 package ie.atu.calc;
 
 public class Calculator {
-    public int add(int first, int second)
-    {
-        return(first + second);
+    public int add(int first, int second) {
+        return (first + second);  //takes in numbers from test file and returns the result
+    }
+
+    public int sub(int first, int second) {
+        return (first - second);
+    }
+
+    public int multiply(int first, int second) {
+        return (first * second);
+    }
+
+    public int divide(int first, int second) {
+        return (first / second);
     }
 }
+
+
+
--- src/test/java/ie/atu/calc/Calculator.java
+++ src/test/java/ie/atu/calc/Calculator.java
@@ -4,13 +4,33 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class CalculatorTest{
+class CalculatorTest {
     Calculator myCalc;
 
-@Test
-    public void testAdd()
-    {
+    @Test
+    public void testAdd() {
         myCalc = new Calculator();
-        assertEquals(40, myCalc.add(20,20));
+        assertEquals(40, myCalc.add(20, 20));
+    }
+
+
+    @Test
+    public void testSub() {
+        myCalc = new Calculator();
+        assertEquals(20, myCalc.sub(100, 80));
+    }
+
+
+    @Test
+    public void testMultiply() {
+        myCalc = new Calculator();
+        assertEquals(25, myCalc.multiply(5, 5));
+    }
+
+
+    @Test
+    public void testDivide() {
+        myCalc = new Calculator();
+        assertEquals(5, myCalc.divide(10, 2));
     }
 }
